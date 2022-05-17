df284abd6536656d365c6850185b4df8c3cc1253
Rodrigo Cremella <rodrigocremella@gmail.com>
2022-05-17 17:43:25 -0300

    update 17-05-22 17:43

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
 import { Landing } from "./Components/index";
-=======
-import Landing from "./Components/index";
 import Home from "./components/Home/Home";
->>>>>>> 3f14c304079ec780f50859734bf1bde3a924bda7
 
 function App() {
   return (
