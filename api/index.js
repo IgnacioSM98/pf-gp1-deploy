bc15fede7eacf8c5f32a645d719da5dff7c8a850
Roque Iván Moyano <rimoyano23@gmail.com>
2022-05-18 15:45:54 -0300

    Modelos y relaciones de la base de datos

--- api/index.js
+++ api/index.js
@@ -21,6 +21,6 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT);
 });
