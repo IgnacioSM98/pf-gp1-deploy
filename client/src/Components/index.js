a43c672678d5a874b6f7ad0218699154e36a6dd1
MoyanoJuan <juan.moyar@gmail.com>
2022-05-18 14:17:13 -0300

    tukiv2, faltan cosas necesito el pull

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -4,5 +4,7 @@ import Blog from "./Blog/Blog";
 import Carrito from "./Carrito/Carrito";
 import Cuenta from "./Cuenta/Cuenta";
 import Tienda from "./Tienda/Tienda";
+import CrearProducto from "./CrearProducto/CrearProducto";
+import NavBar from "./NavBar/NavBar";
 
-export { Landing, Home, Blog, Carrito, Cuenta, Tienda };
+export { Landing, Home, Blog, Carrito, Cuenta, Tienda, CrearProducto, NavBar };
