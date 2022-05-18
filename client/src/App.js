0efaf2dba412b29f0d50a3b6566b3cb486dc4b28
MoyanoJuan <juan.moyar@gmail.com>
2022-05-18 12:10:53 -0300

    NavBar/inicial

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
-import { Landing, Home } from "./Components/index";
+import {
+  Landing,
+  Home,
+  Tienda,
+  Blog,
+  Cuenta,
+  Carrito,
+} from "./Components/index";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <Home />
+      <Routes>
+        {/*<Route exact path="/" element={<Landing />} />*/}
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/tienda" element={<Tienda />} />
+        <Route exact path="/tienda/:id" element={"Detalles"} />
+        <Route exact path="/carrito" element={<Carrito />} />
+        <Route exact path="/blog" element={<Blog />} />
+        <Route exact path="/usuario" element={<Cuenta />} />
+        <Route exact path="/admin" element={"admin"} />
+        <Route exact path="/admin/crear" element={"form de crear producto"} />
+        {/*<Route exact path="/user/reviews" element={"user reviews"} />
+        <Route exact path="/admin/cambiar/:id" element={"change something"} />*/}
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Components/Blog/Blog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Blog() {
+  return <span>Blog</span>;
+}
+
+export default Blog;
