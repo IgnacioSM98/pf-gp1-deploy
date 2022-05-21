59b07eb1ef4b237b1badb6732b379b0f344bcd9b
Ignacio_SM <92553750+IgnacioSM98@users.noreply.github.com>
2022-05-21 14:41:49 -0300

    Update DetalleProducto.jsx

--- client/src/Components/DetalleProducto/DetalleProducto.jsx
+++ client/src/Components/DetalleProducto/DetalleProducto.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getDetail } from "../../Redux/actions";
 import Reviews from "../Reviews/Reviews";
 import { clearDetail, getDetail } from "../../Redux/actions";
 import styled from "styled-components";
