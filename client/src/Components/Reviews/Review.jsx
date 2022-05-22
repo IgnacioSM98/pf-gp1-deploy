50bc0c59986444e7a00ddac894ef4cd4a4191173
Rodrigo Cremella <rodrigocremella@gmail.com>
2022-05-22 20:01:59 -0300

    update 22-05-22 20:01

--- client/src/Components/Reviews/Review.jsx
+++ client/src/Components/Reviews/Review.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import "./Review.css";
-import { StarRating } from "../index";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 140px;
+  width: 300px;
+  background-color: grey;
+  margin: 2px;
+  border-radius: 10px;
+
+  font-size: 17px;
+`;
+
+const Stars = styled.div`
+  display: flex;
+  color: white;
+  font-size: 17px;
+`;
 
 export default function Review({ puntaje, comentario }) {
   return (
-    <div className="container-review">
-      <StarRating />
+    <Container>
+      <Stars>
+        {[...Array(5)].map((star, index) => (
+          <span style={{ textShadow: "1px 1px black" }} key={index}>
+            &#9733;
+          </span>
+        ))}
+      </Stars>
+
       <div className="puntaje-review">
         <h3>{puntaje}</h3>
       </div>
@@ -13,6 +37,6 @@ export default function Review({ puntaje, comentario }) {
       <div className="comentario-review">
         <p>{comentario}</p>
       </div>
-    </div>
+    </Container>
   );
 }
