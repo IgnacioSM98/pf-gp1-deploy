d4c3fbe93e86f558a91903ce4a3eb2e72e372497
Fatimalezcano <fatimalezcanodev@gmail.com>
2022-06-01 16:59:35 -0300

    funcionalidad favoritos

--- client/src/Components/Producto/Producto.jsx
+++ client/src/Components/Producto/Producto.jsx
@@ -10,7 +10,9 @@ import {
   restarCarrito,
   añadirAFavoritos,
   eliminarDeFavoritos,
+  getUsuarios,
 } from "../../Redux/actions";
+import axios from "axios";
 
 const LinkProduct = styled(Link)`
   text-decoration: none;
@@ -18,7 +20,30 @@ const LinkProduct = styled(Link)`
 `;
 
 const Button = styled.button`
-  height: 30px;
+  height: 25px;
+  width: 90%;
+  margin: 2px 5%;
+  background-color: white;
+  border: none;
+
+  cursor: pointer;
+`;
+
+const Popup = styled.div`
+  position: absolute;
+  top: 3%;
+  right: 7%;
+  width: 22px;
+  height: 22px;
+  border-radius: 50px;
+  border: none;
+  z-index: 11;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 700;
+  background-color: #36885ed1;
+  color: white;
+  cursor: auto;
 `;
 
 const Botones = styled.div`
@@ -57,13 +82,14 @@ const Stock = styled.button`
 `;
 
 const Boton = styled.button`
-  color: #222;
+  color: #36885ed1;
   border-radius: 10px 10px 10px 10px;
   height: 35px;
   width: 100px;
   background-color: white;
   font-family: Poppins;
   font-size: 13px;
+  font-weight: 800;
   box-shadow: 0 2px 2px 0 #222, 0 2px 2px 0 #222;
   border: none;
   outline: none;
@@ -74,7 +100,7 @@ const Boton = styled.button`
   right: 10px;
 
   &:hover {
-    background-color: #37563de0;
+    background-color: #36885ed1;
     color: white;
     cursor: pointer;
   }
@@ -107,19 +133,19 @@ const ContenedorFav = styled.div`
   display: flex;
   position: absolute;
   z-index: 3;
-  left: 87%;
+  left: 7%;
   top: 2%;
 
   p {
     color: #5a9d7b;
-    font-size: 30px;
-    margin-top: 0;
+    font-size: 25px;
+    margin-top: 5px;
   }
 
   label {
     color: #5a9d7b;
     font-weight: bold;
-    font-size: 22px;
+    font-size: 25px;
 
     margin-top: 5px;
   }
@@ -152,6 +178,7 @@ export default function Producto({
   );
 
   useEffect(() => {
+    dispatch(getUsuarios());
     setFlag(cantidadCarrito?.cantidad ? true : false);
     setCantidad(cantidadCarrito?.cantidad ? cantidadCarrito.cantidad : 1);
   }, [cantidadCarrito]);
@@ -199,7 +226,6 @@ export default function Producto({
     } else {
       dispatch(añadirAFavoritos(producto));
     }
-    console.log(favoritos);
   }
 
   return (
@@ -226,39 +252,34 @@ export default function Producto({
         </div>
 
         {showOptions.button && (
-          <button
-            style={{
-              position: "absolute",
-              top: "5px",
-              right: "5px",
-              width: "30px",
-              height: "30px",
-              borderRadius: "50px",
-              border: "none",
-              zIndex: "2",
-            }}
+          <Popup
+            style={
+              showOptions.popup
+                ? { backgroundColor: "white", color: "#36885ed1" }
+                : null
+            }
             onClick={(e) => {
               e.preventDefault();
               setOptions({ ...showOptions, popup: !showOptions.popup });
             }}
           >
             ...
-          </button>
+          </Popup>
         )}
 
         {showOptions.popup && (
           <div
             style={{
               position: "absolute",
-              top: "3px",
+              top: "12px",
               left: "1%",
               right: "1%",
               width: "98%",
-              height: "130px",
+              height: "100px",
               borderRadius: "12px",
               border: "none",
-              backgroundColor: "grey",
-              zIndex: "1",
+              backgroundColor: "#36885ed1",
+              zIndex: "10",
               display: "flex",
               flexDirection: "column",
             }}
@@ -277,7 +298,7 @@ export default function Producto({
 
         <div className="descripcion">
           <p>
-            {descripcion.length > 210
+            {descripcion?.length > 210
               ? descripcion.slice(0, 210) + " (Ver más)"
               : descripcion}
           </p>
@@ -312,7 +333,7 @@ export default function Producto({
                 agregarAlCarrito(e);
               }}
             >
-              AGREGAR
+              Agregar
             </Boton>
           )}
         </div>
