7dc93e8eb3fcc600f15be4b269036d115c5ec46c
Fatimalezcano <fatimalezcanodev@gmail.com>
2022-06-07 12:08:40 -0300

    medio carrusel

---
+++ app/infusion-store/sources/Components/Carousel/Data.js
@@ -0,0 +1,30 @@
+const imagen1 = require("./CarouselImagenes/imagen01.png");
+const imagen2 = require("./CarouselImagenes/imagen02.png");
+const imagen3 = require("./CarouselImagenes/imagen03.png");
+const imagen4 = require("./CarouselImagenes/imagen04.png");
+const imagen5 = require("./CarouselImagenes/imagen05.png");
+const imagen6 = require("./CarouselImagenes/imagen06.png");
+const imagen7 = require("./CarouselImagenes/imagen07.png");
+const imagen8 = require("./CarouselImagenes/imagen08.png");
+
+export const imagenes = [
+  imagen1,
+  imagen2,
+  imagen3,
+  imagen4,
+  imagen5,
+  imagen6,
+  imagen7,
+  imagen8,
+];
+
+// export const imagenes = [
+//   { imagen: "./CarouselImagenes/imagen01.png" },
+//   { imagen: "./CarouselImagenes/imagen02.png" },
+//   { imagen: "./CarouselImagenes/imagen03.png" },
+//   { imagen: "./CarouselImagenes/imagen04.png" },
+//   { imagen: "./CarouselImagenes/imagen05.png" },
+//   { imagen: "./CarouselImagenes/imagen06.png" },
+//   { imagen: "./CarouselImagenes/imagen07.png" },
+//   { imagen: "./CarouselImagenes/imagen08.png" },
+// ];
