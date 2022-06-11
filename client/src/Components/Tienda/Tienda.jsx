22f998422d2aa44482a2371b6a3c568f526fd6dc
Rodrigo Cremella <rodrigocremella@hotmail.com>
2022-06-11 17:23:37 -0300

    update 11-06-22 17:23

--- client/src/Components/Tienda/Tienda.jsx
+++ client/src/Components/Tienda/Tienda.jsx
@@ -218,8 +218,13 @@ function Shop({ contacto }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const productos = useSelector((state) => state.productos);
-  const productosFiltrados = useSelector((state) => state.productosFiltrados);
+  const productos = useSelector((state) =>
+    state.productos.filter((producto) => producto.stock)
+  );
+
+  const productosFiltrados = useSelector((state) =>
+    state.productosFiltrados.filter((producto) => producto.stock)
+  );
 
   const user = useSelector((state) => state.userInfo?.visualizacion);
   const userInfo = useSelector((state) => state.userInfo);
@@ -247,10 +252,11 @@ function Shop({ contacto }) {
   }, [resVis]);
 
   useEffect(() => {
-    setPages(Math.ceil(productos.filter(filterStock).length / 9));
-  }, [productos]);
+    setPages(Math.ceil(productosFiltrados.length / 9));
+  }, [productosFiltrados]);
 
   useEffect(() => {
+    setPageSelected(1);
     setPages(Math.ceil(productosFiltrados.filter(filterDropdown).length / 9));
     // eslint-disable-next-line
   }, [selected]);
@@ -267,7 +273,7 @@ function Shop({ contacto }) {
     }
 
     // eslint-disable-next-line
-    const arrayAux = productos.filter(filterStock).filter((producto) => {
+    const arrayAux = productos.filter((producto) => {
       const name = producto.nombre.toLowerCase();
       const isVisible = name.includes(value.toLowerCase());
 
@@ -281,9 +287,9 @@ function Shop({ contacto }) {
     dispatch(getProductosFiltrados(arrayAux));
   }
 
-  const filterStock = (producto) => {
-    if (producto.stock > 0) return producto;
-  };
+  // const filterStock = (producto) => {
+  //   if (producto.stock > 0) return producto;
+  // };
 
   const filterPerPages = (producto, i) => {
     if (i >= 9 * (pageSelected - 1) && i <= 9 * pageSelected - 1) {
@@ -354,7 +360,7 @@ function Shop({ contacto }) {
 
             {productosFiltrados &&
               productosFiltrados
-                .filter(filterStock)
+                // .filter(filterStock)
                 .filter(filterDropdown)
                 .filter(filterPerPages)
                 .map((el) => {
@@ -372,7 +378,6 @@ function Shop({ contacto }) {
                       producto={el}
                     />
                   );
-                  // }
                 })}
           </ProductosTienda>
 
